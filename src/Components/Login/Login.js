639c77cd5b55fc486506ba62870ed9119cfec1b6
Matheus Proença <mproenca.marketing@gmai.com>
2021-12-23 14:01:15 -0300

    Estrutura inicial

---
+++ src/Components/Login/Login.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Styles from './Login.module.css'
+
+const Login = () => {
+  return (
+    <div className={Styles.login}>
+      Login
+    </div>
+  )
+}
+
+export default Login;
