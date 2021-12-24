6e59346b2824cb45610888ed4adee14417d5da7d
Matheus Proença <mproenca.marketing@gmai.com>
2021-12-24 20:51:14 -0300

    login-routes

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,10 +1,20 @@
 import React from 'react'
-import Styles from './Login.module.css'
+import { Route, Routes } from 'react-router-dom';
+import LoginForm from './LoginForm'
+import styles from './Login.module.css'
+import LoginPasswordLost from './LoginPasswordLost';
+import LoginPasswordReset from './LoginPasswordReset';
+import LoginCreate from './LoginCreate';
 
 const Login = () => {
   return (
-    <div className={Styles.login}>
-      Login
+    <div className={styles.login}>
+      <Routes>
+        <Route path="/" element={<LoginForm />}/>
+        <Route path="/criar" element={<LoginCreate />}/>
+        <Route path="/perdeu" element={<LoginPasswordLost />}/>
+        <Route path="/resetar" element={<LoginPasswordReset />}/>
+      </Routes>
     </div>
   )
 }
