5a1c9ab3929fa3ba5f15424b1754c1b63cddd63f
Matheus Proença <mproenca.marketing@gmai.com>
2021-12-27 17:51:12 -0300

    use-form

--- src/Components/Forms/Input.js
+++ src/Components/Forms/Input.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import styles from './Input.module.css';
 
-const Input = ({ label, type, name }) => {
+const Input = ({ label, type, name, value, onChange, error, onBlur }) => {
   return (
     <div className={ styles.wrapper }>
       <label className={ styles.label } htmlFor={ name }>{ label }</label>
@@ -10,8 +10,11 @@ const Input = ({ label, type, name }) => {
         type={ type }
         id={ name }
         name={ name }
+        value={ value }
+        onChange={ onChange }
+        onBlur={onBlur}
       />
-      <p className={styles.error}>Error</p>
+      {error && <p className={styles.error}>{ error }</p>}
     </div>
   )
 }
