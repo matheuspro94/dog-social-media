b7ecbd9996e780c942bde6c7c7e0f27fd39c1cac
mroenca40 <mproenca.marketing@gmail.com>
2022-01-15 20:19:35 -0300

    adiciona estilo no login

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,25 +1,27 @@
 import React from 'react'
 import { Route, Routes, Navigate } from 'react-router-dom';
 import LoginForm from './LoginForm'
-import styles from './Login.module.css'
 import LoginPasswordLost from './LoginPasswordLost';
 import LoginPasswordReset from './LoginPasswordReset';
 import LoginCreate from './LoginCreate';
 import { UserContext } from '../../UserContext';
+import styles from './Login.module.css'
 
 const Login = () => {
   const { login } = React.useContext(UserContext);
 
   if (login === true) return <Navigate to="/conta" />
   return (
-    <div className={styles.login}>
-      <Routes>
-        <Route path="/" element={<LoginForm />}/>
-        <Route path="/criar" element={<LoginCreate />}/>
-        <Route path="/perdeu" element={<LoginPasswordLost />}/>
-        <Route path="/resetar" element={<LoginPasswordReset />}/>
-      </Routes>
-    </div>
+    <section className={styles.login}>
+      <div className={styles.forms}>
+        <Routes>
+          <Route path="/" element={<LoginForm />}/>
+          <Route path="/criar" element={<LoginCreate />}/>
+          <Route path="/perdeu" element={<LoginPasswordLost />}/>
+          <Route path="/resetar" element={<LoginPasswordReset />}/>
+        </Routes>
+      </div>
+    </section>
   )
 }
 
