3b94f441576b5017baa8dd4825d731ad515fb594
mroenca40 <mproenca.marketing@gmail.com>
2022-05-30 16:36:03 -0300

    add protected route

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Footer from './Components/Footer';
 import Header from './Components/Header';
 import Home from './Components/Home';
 import Login from './Components/Login/Login';
+import User from './Components/User/User';
+import ProtectedRoute from './Components/Helper/ProtectedRoute';
 import { UserStorage } from './UserContext'
 
 
@@ -16,7 +18,10 @@ function App() {
           <Header />
             <Routes>
               <Route path='/' element={ <Home /> }/>
-              <Route path='/login/*' element={ <Login /> }/>
+              <Route path='login/*' element={ <Login /> }/>
+              <Route path='conta/*' element={
+                <ProtectedRoute><User /></ProtectedRoute>
+              }/>
             </Routes>
           <Footer />
         </UserStorage>
