df604ded254836b336754b3f07c8cd663ff143cb
mroenca40 <mproenca.marketing@gmail.com>
2022-06-28 16:24:43 -0300

    add feed

---
+++ src/Components/Feed/FeedPhotos.js
@@ -0,0 +1,34 @@
+import React from 'react'
+
+import { PHOTOS_GET } from '../../Api'
+import useFetch from '../../Hooks/useFetch'
+import Error from '../Helper/Error'
+import FeedPhotosItem from './FeedPhotosItem'
+import Loading from '../Helper/Loading'
+import styles from './FeedPhotos.module.css'
+
+const FeedPhotos = () => {
+
+  const { data, loading, error, request } = useFetch()
+
+  React.useEffect(() => {
+    async function fetchPhoto() {
+      const { url, options } = PHOTOS_GET({ page: 1, total: 6, user: 0 })
+      const { response, json } = await request(url, options)
+      console.log(json);
+    }
+    fetchPhoto()
+  }, [request])
+
+  if (error) return <Error error={error} />
+  if (loading) return <Loading />
+  if (data)
+    return (
+      <ul className={`${styles.feed} animeLeft`}>
+        {data.map(photo => <FeedPhotosItem key={photo.id} photo={photo} />)}
+      </ul>
+    )
+  else return null
+}
+
+export default FeedPhotos
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import Feed from './Feed/Feed';
 
 const Home = () => {
-  return <div>Home</div>;
+  return <section className="container mainContainer">
+    <Feed />
+  </section>;
 };
 
 export default Home;
